708cedc71e6f6bf83483d07603045c99dd5f9a05
Veyyr <dimapolska967@gmail.com>
2026-09-21 20:50:25 +0500

    task 1 almost

--- main.kt
+++ main.kt
@@ -1,4 +1,19 @@
 fun main() {
-    val fruits = "apple,banana,orange"
-    println(fruits.split(","))
+    task1()
+}
+
+fun task1() {
+    // Создать массив из 5 целых чисел и вывести все элементы.
+    val numbers = arrayOf(1, 2, 3, 4, 5)
+    for (number in numbers) {
+        println(number)
+    }
+
+    // Найти сумму элементов массива.
+    val summ = intArrayOf(5, 10, 15, 20)
+    println(summ.sum())
+
+    // Найти максимальный элемент массива.
+    val maxnum = intArrayOf(5, 10, 15, 20)
+    println(maxnum.maxOrNull())
 }
